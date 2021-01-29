7ed6bb4465e627102621ac5d1ea0c5589a09b378
feli1407 <78106210+feli1407@users.noreply.github.com>
2021-01-29 11:13:18 -0300

    Add files via upload

--- 1-EntradaSalida/jsEntradaSalida-01.js
+++ 1-EntradaSalida/jsEntradaSalida-01.js
@@ -3,6 +3,5 @@ Al presionar el  botón,
 se debe mostrar un mensaje como el siguiente "Esto funciona de maravilla"*/
 function mostrar()
 {
-	alert("hola bienvenido felipe");
+	alert("Esto funciona de maravilla");
 }
-
