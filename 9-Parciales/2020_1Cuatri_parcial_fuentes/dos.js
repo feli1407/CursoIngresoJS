03d98cd6aaf97fb0af01d3a0d4e092c1f49abcb4
carlos casco <felicasco1403@gmail.com>
2021-02-24 20:45:19 -0300

    parciales 1 y 2

--- 9-Parciales/2020_1Cuatri_parcial_fuentes/dos.js
+++ 9-Parciales/2020_1Cuatri_parcial_fuentes/dos.js
@@ -1,4 +1,181 @@
+/*Realizar el algoritmo que permita ingresar los datos de una compra productos de la construccion, hasta que el cliente quiera:
+Tipo validad("arena";"cal";"cemento") 
+Cantidad de bolsas,
+Precio por bolsa (más de cero ), 
+Si compro más de 10 bolsas en total tenes 15% de descuento sobre el total a pagar.
+Si compro más de 30 bolsas en total tenes 25% de descuento sobre el total a pagar.	
+a)El importe total a pagar , bruto sin descuento y...
+b)el importe total a pagar con descuento(solo si corresponde)
+d)Informar el tipo con mas cantidad de bolsas.		
+f)El tipo mas caro
+*/
+
 function mostrar()
 {
-  alert("dos");
+  let tipoingresado;
+  let cantidaddebolsas;
+  let precioporbolsa;
+  let respuesta;
+  let preciomenosde10unidades;
+  let preciototal;
+  let preciototalcon15descuento;
+  let preciototalcon25descuento;
+  let mensajedescuento;
+  let acumuladorarena;
+  let acumuladorcal;
+  let acumuladorcemento;
+  let mensajecantidad;
+  let precioarena;
+  let preciocal;
+  let preciocemento;
+  let contadordearena;
+  let contadordecal;
+  let contadordecemento;
+  let tipomascaro;
+  let acumuladordebolsas;
+
+  preciototal=0;
+  acumuladordebolsas=0;
+  contadordearena=0;
+  contadordecal=0;
+  contadordecemento=0;
+  precioarena=0;
+  preciocal=0;
+  preciocemento=0;
+  acumuladorarena=0;
+  acumuladorcal=0;
+  acumuladorcemento=0;
+
+
+  do
+  {
+    tipoingresado = prompt("ingrese tipo");
+    while(isNaN(tipoingresado) == false && tipoingresado != "arena" && tipoingresado != "cal" && tipoingresado != "cemento")
+    {
+      tipoingresado = prompt("error, ingrese tipo");
+    }
+
+    cantidaddebolsas = prompt("ingrese cantidad");
+    cantidaddebolsas = parseInt(cantidaddebolsas);
+    while(isNaN(cantidaddebolsas) == true && cantidaddebolsas<1)
+    {
+      cantidaddebolsas = prompt("error, ingrese cantidad");
+      cantidaddebolsas = parseInt(cantidaddebolsas);
+    }
+
+    precioporbolsa = prompt("ingrese precio");
+    precioporbolsa = parseInt(precioporbolsa);
+    while(isNaN(precioporbolsa) == true && cantidaddebolsas<1)
+    {
+      precioporbolsa = prompt("error, ingrese precio");
+      precioporbolsa = parseInt(precioporbolsa);
+    }
+
+preciototal = (cantidaddebolsas * precioporbolsa) + preciototal;
+acumuladordebolsas = acumuladordebolsas + cantidaddebolsas;
+
+    //Si compro más de 10 bolsas en total tenes 15% de descuento sobre el total a pagar.
+
+    if(acumuladordebolsas>10 && acumuladordebolsas<30)
+    {
+      preciototalcon15descuento = preciototal * 0.85;
+      mensajedescuento = " usted tiene un 15% de descuento entonces debera pagar: "+preciototalcon15descuento;
+    }
+
+    else if(acumuladordebolsas>30)
+    {
+      preciototalcon25descuento = preciototal * 0.75;
+      mensajedescuento = " usted tiene un 25% de descuento entonces debera pagar: "+preciototalcon25descuento;
+    }
+    else
+    {
+      preciomenosde10unidades = preciototal;
+      mensajedescuento = " usted no obtiene ningun descuento";
+    }
+
+    switch(tipoingresado)
+    {
+      case "arena":
+        acumuladorarena = acumuladorarena + cantidaddebolsas;
+        if(contadordearena == 0)
+        {
+           precioarena = precioporbolsa;
+        }
+        else
+        {
+           if(precioporbolsa>precioarena)
+          {
+            precioarena = precioporbolsa;
+          }
+        }
+        contadordearena++;
+        break;
+      case "cal":
+        acumuladorcal = acumuladorcal + cantidaddebolsas;
+        if(contadordecal == 0)
+        {
+          preciocal = precioporbolsa;
+        }
+        else
+        {
+          if(precioporbolsa>preciocal)
+          {
+          preciocal = precioporbolsa;
+          }
+        }
+        contadordecal++;
+        break;
+      case "cemento":
+        acumuladorcemento = acumuladorcemento + cantidaddebolsas;
+        if(contadordecemento == 0)
+        {
+          preciocemento = precioporbolsa;
+        }
+        
+        {
+          if(precioporbolsa>preciocemento)
+          {
+          preciocemento = precioporbolsa;
+          }
+        }
+        contadordecemento++;
+        break;
+    }
+
+    respuesta = prompt("desea seguir comprando?");
+  }while(respuesta == "si");
+
+  if(acumuladorarena>acumuladorcal && acumuladorarena>acumuladorcemento)
+  {
+    mensajecantidad = "las bolsas que mas se compraron fueron las de arena";
+  }
+  else if(acumuladorcal>acumuladorarena && acumuladorcal>acumuladorcemento)
+  {
+    mensajecantidad = "las bolsas que mas se compraron fueron las de cal";
+  }
+  else if(acumuladorcemento>acumuladorcal && acumuladorcemento>acumuladorarena)
+  {
+    mensajecantidad = "las bolsas que mas se compraron fueron las de cemento";
+  }
+
+  if(preciocemento>precioarena && preciocemento>preciocal)
+  {
+    tipomascaro = "el tipo mas caro es cemento";
+  }
+  else if(preciocal>preciocemento && preciocal>precioarena)
+  {
+    tipomascaro = "el tipo mas caro es cal";
+  }
+  else if(precioarena>preciocemento && precioarena>preciocal)
+  {
+    tipomascaro = "el tipo mas caro es arena";
+  }
+
+
+
+alert("el importe total a pagar es: "+preciototal+mensajedescuento);
+alert(mensajecantidad);
+alert(tipomascaro);
+
+
 }
--- 9-Parciales/2020_1Cuatri_parcial_fuentes/uno.js
+++ 9-Parciales/2020_1Cuatri_parcial_fuentes/uno.js
@@ -1,5 +1,145 @@
+/*	
+Debemos realizar la carga de 5(cinco) productos de prevención de contagio, 
+de cada una debo obtener los siguientes datos: 
+el tipo (validar "barbijo" , "jabón" o "alcohol") , 
+el precio (validar entre 100 y 300),
+la cantidad de unidades (no puede ser 0 o negativo y no debe superar las 1000 unidades), 
+la Marca y el fabricante. 
+Se debe Informar al usuario lo siguiente:
 
+a) Del más barato de los alcohol, la cantidad de unidades y el fabricante
+b) Del tipo con mas unidades, el promedio por compra 
+c) Cuántas unidades de jabones hay en total 
+*/
 function mostrar()
 {
-	alert("uno");
+let contadordevueltas;
+let tipoingresado;
+let precioingresado;
+let cantidaddeunidades;
+let marcaingresada;
+let fabricanteingresado;
+let alcoholmasbarato;
+let cantidaddeunidadesalcoholbarato;
+let fabricantealcoholbarato;
+//let flagprimeralcohol;
+let contadordealcohol;
+let contadordebarbijo;
+let contadordejabon;
+let acumuladordealcohol;
+let acumuladordebarbijo;
+let acumuladordejabon;
+let promedio;
+let mensaje;
+
+promedio=0;
+contadordealcohol=0;
+contadordebarbijo=0;
+contadordejabon=0;
+acumuladordealcohol=0;
+acumuladordebarbijo=0;
+acumuladordejabon=0;
+flagprimeralcohol=true;
+contadordevueltas=0;
+
+while(contadordevueltas<5)
+{
+tipoingresado = prompt("ingrese tipo");
+    while(isNaN(tipoingresado) == false && tipoingresado!="barbijo" && tipoingresado!="jabon" && tipoingresado!="alcohol")
+    {
+		tipoingresado = prompt("Error, ingrese tipo");
+    }
+
+precioingresado = prompt("ingrese precio");
+precioingresado = parseInt(precioingresado);
+    while(isNaN(precioingresado) == true && precioingresado<100 || precioingresado>300)
+	{
+		precioingresado = prompt("Error, ingrese precio");
+		precioingresado = parseInt(precioingresado);
+	}
+
+cantidaddeunidades = prompt("ingrese cantidad");
+cantidaddeunidades = parseInt(cantidaddeunidades);
+    while(isNaN(cantidaddeunidades) == true && cantidaddeunidades<=0 || cantidaddeunidades>1000)
+	{
+		cantidaddeunidades = prompt("ingrese cantidad");
+		cantidaddeunidades = parseInt(cantidaddeunidades);
+	}
+
+marcaingresada = prompt("ingrese marca");
+    while(isNaN(marcaingresada) == false)
+    {
+		marcaingresada = prompt("Error, ingrese marca");
+    }
+
+fabricanteingresado = prompt("ingrese fabricante");
+    while(isNaN(fabricanteingresado) == false)
+    {
+		fabricanteingresado = prompt("Error, ingrese fabricante");
+    }
+
+//a) Del más barato de los alcohol, la cantidad de unidades y el fabricante
+
+if(tipoingresado == "alcohol" )
+{
+	//if(flagprimeralcohol == true)
+	if(contadordealcohol == 0)
+	{
+		precioingresado = alcoholmasbarato;
+		cantidaddeunidadesalcoholbarato = cantidaddeunidades;
+		fabricantealcoholbarato = fabricanteingresado;
+		//flagprimeralcohol = false;
+	}
+	else
+	{
+		if(precioingresado<alcoholmasbarato)
+		{
+			precioingresado = alcoholmasbarato;
+			cantidaddeunidadesalcoholbarato = cantidaddeunidades;
+		    fabricantealcoholbarato = fabricanteingresado;
+		}
+	}
+}
+
+//b) Del tipo con mas unidades, el promedio por compra
+switch(tipoingresado)
+{
+	case "alcohol":
+		contadordealcohol++;
+		acumuladordealcohol = acumuladordealcohol + cantidaddeunidades;
+		break;
+	case "jabon":
+		contadordejabon++;
+		acumuladordejabon = acumuladordejabon + cantidaddeunidades;
+		break;
+	case "barbijos":
+		contadordealcohol++;
+		acumuladordealcohol = acumuladordealcohol + cantidaddeunidades;
+		break;	
+}
+
+contadordevueltas++;
+}
+
+if(acumuladordealcohol>acumuladordejabon && acumuladordealcohol>acumuladordebarbijo)
+{
+    promedio = acumuladordealcohol / contadordealcohol;
+    mensaje = "el promedio por compra de alcohol es: "+promedio;
+}
+else if(acumuladordejabon>acumuladordealcohol && acumuladordejabon>acumuladordebarbijo)
+{
+	promedio = acumuladordejabon/contadordejabon;
+    mensaje = "el promedio por compra de jabon es: "+promedio;
+}
+else if(acumuladordebarbijo>acumuladordealcohol && acumuladordebarbijo>acumuladordejabon)
+{
+	promedio = acumuladordebarbijo / contadordebarbijo;
+	mensaje = "el promedio por compra de jabon es: "+promedio;
+}
+
+//c) Cuántas unidades de jabones hay en total
+alert("hay "+acumuladordejabon+" unidades de jabon en total.");
+alert(mensaje);
+alert("la cantidad de unidades del alcohol mas barato: "+cantidaddeunidades+ " y el fabricante es: "+fabricanteingresado);
+
 }
